10e8a76e640949d24711662f6d2a7c1f09a41bc0
Ali El Cheikh Ali <58534434+Ali-ElCheikh@users.noreply.github.com>
2022-02-11 14:56:17 -0500

    Add files via upload

---
+++ police_stations.js
@@ -0,0 +1,9 @@
+//StAuth10244: I Ali El Cheikh Ali, 000760145 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
+var police_stations = [
+{ "type": "Feature", "properties": { "OBJECTID": 1, "NAME": "Central Area", "LONGITUDE": "-79.86258986960881", "LATITUDE": "43.25622360362614", "ADDRESS": "155 King William Street", "COMMUNITY": "Hamilton", "STATION": 10 }, "geometry": { "type": "Point", "coordinates": [ -79.862589869608726, 43.256223602689609 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 2, "NAME": "East Hamilton and Stoney Creek", "LONGITUDE": "-79.77098238190851", "LATITUDE": "43.220408870192166", "ADDRESS": "2825 King Street East", "COMMUNITY": "Hamilton", "STATION": 20 }, "geometry": { "type": "Point", "coordinates": [ -79.770982381908425, 43.220408869255728 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 3, "NAME": "Mountain and Glanbrook", "LONGITUDE": "-79.87651770480485", "LATITUDE": "43.19794615177183", "ADDRESS": "400 Rymal Road East", "COMMUNITY": "Hamilton", "STATION": 30 }, "geometry": { "type": "Point", "coordinates": [ -79.876517704804769, 43.197946150835406 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 4, "NAME": "Dundas Station", "LONGITUDE": "-79.95404098643385", "LATITUDE": "43.26591221945497", "ADDRESS": "2 King Street West", "COMMUNITY": "Dundas", "STATION": 31 }, "geometry": { "type": "Point", "coordinates": [ -79.954040986433782, 43.26591221851843 ] } }
+];
+
